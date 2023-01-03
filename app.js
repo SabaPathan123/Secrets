14b829f048b1b4aab68aba0e42b1464edc1129f0
Saba Pathan <shaikh.saba88@gmail.com>
2023-01-03 21:56:52 +0530

    Level - 1 Username & Password authentication

--- app.js
+++ app.js
@@ -8,23 +8,64 @@ mongoose.set('strictQuery', false);
 const app = express();
 
 app.use(express.static("public"));
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
-app.get("/", function(request,response){
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(request, response) {
   response.render("home");
 });
 
-app.get("/login", function(request,response){
+app.get("/login", function(request, response) {
   response.render("login");
 });
 
-app.get("/register", function(request,response){
+app.get("/register", function(request, response) {
   response.render("register");
 });
 
-app.listen(3000, function(){
+app.post("/register", function(request, response) {
+  const newUser = new User({
+    email: request.body.username,
+    password: request.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.render('secrets');
+    }
+  });
+});
+
+app.post("/login", function(request, response) {
+  const username = request.body.username;
+  const password = request.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, user) {
+    if (user.password == password) {
+      response.render("secrets");
+    } else {
+      response.redirect("/login");
+    }
+  });
+});
+
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
