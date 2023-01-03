7031e44c3aa0fb93af21db13f469d324883d8972
Saba Pathan <shaikh.saba88@gmail.com>
2023-01-03 21:18:31 +0530

    Connect to database

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended : true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true});
 
 app.get("/", function(request,response){
   response.render("home");
